c824b4a3766db5779c78cd39a4b00aa72c2ff63e
zhixin9001 <zhixin9001@126.com>
2017-03-28 08:07:58 +0800

    Added CommonHelper and Test project

---
+++ Tests/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZSZ.Common;
+
+namespace Tests
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var s = CommonHelper.GenerateCaptchaCode(4);
+      Console.WriteLine(s);
+      Console.ReadKey();
+    }
+  }
+}
---
+++ ZSZ.Common/CommonHelper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZSZ.Common
+{
+  public class CommonHelper
+  {
+    #region MD5
+    public static string CalcMD5(string str)
+    {
+      byte[] bytes = System.Text.Encoding.UTF8.GetBytes(str);
+      return CalcMD5(bytes);
+    }
+
+    public static string CalcMD5(byte[] bytes)
+    {
+      using (MD5 md5 = MD5.Create())
+      {
+        byte[] computeBytes = md5.ComputeHash(bytes);
+        string result = "";
+        for (int i = 0; i < computeBytes.Length; i++)
+        {
+          result += computeBytes[i].ToString("X").Length == 1 ? "0" + computeBytes[i].ToString("X") : computeBytes[i].ToString("X");
+        }
+        return result;
+      }
+    }
+
+    public static string CalcMD5(Stream stream)
+    {
+      using (MD5 md5 = MD5.Create())
+      {
+        byte[] computeBytes = md5.ComputeHash(stream);
+        string result = "";
+        for (int i = 0; i < computeBytes.Length; i++)
+        {
+          result += computeBytes[i].ToString("X").Length == 1 ? "0" + computeBytes[i].ToString("X") : computeBytes[i].ToString("X");
+        }
+        return result;
+      }
+    }
+    #endregion
+
+    #region Captcha
+    public static string GenerateCaptchaCode(int len)
+    {
+      char[] data = { 'a', 'c', 'd', 'e', 'f', 'g', 'k', 'm', 'p', 'r', 's', 't', 'w', 'x', 'y', '3', '4', '5', '7', '8' };
+      StringBuilder sbCode = new StringBuilder();
+      Random rand = new Random();
+      for (int i = 0; i < len; i++)
+      {
+        char ch = data[rand.Next(data.Length)];
+        sbCode.Append(ch);
+      }
+      return sbCode.ToString();
+    }
+
+    #endregion
+
+
+  }
+}
