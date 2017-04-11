4d62ff616af6ddde79b664be6b5c9f293e7dbd48
zhixin9001 <zhixin9001@126.com>
2017-04-11 14:12:21 +0800

    add ut

--- ZSZ.ServiceTest/TestAdminUserService.cs
+++ ZSZ.ServiceTest/TestAdminUserService.cs
@@ -9,24 +9,24 @@ namespace ZSZ.ServiceTest
   [TestClass]
   public class TestAdminUserService
   {
-    private AdminUserService adminUserService = new AdminUserService();
+    private AdminUserService auService = new AdminUserService();
     [TestMethod]
     public void TestAddAndLogin()
     {
-      long id = adminUserService.AddAdminUser("abc", "134133", "adsf", "s", null);
-      var admin = adminUserService.GetById(id);
+      long id = auService.AddAdminUser("abc", "134133", "adsf", "s", null);
+      var admin = auService.GetById(id);
       Assert.AreEqual(admin.Name, "abc");
       Assert.AreEqual(admin.PhoneNum, "134133");
       Assert.AreEqual(admin.Email, "s");
       Assert.IsNull(admin.CityId);
-      Assert.IsTrue(adminUserService.CheckLogin("134133", "adsf"));
-      Assert.IsFalse(adminUserService.CheckLogin("134133", "sadsf"));
-      adminUserService.GetAll();
-      Assert.IsNotNull(adminUserService.GetByPhoneNum("134133"));
-      adminUserService.MarkDeleted(id);
+      Assert.IsTrue(auService.CheckLogin("134133", "adsf"));
+      Assert.IsFalse(auService.CheckLogin("134133", "sadsf"));
+      auService.GetAll();
+      Assert.IsNotNull(auService.GetByPhoneNum("134133"));
+      auService.MarkDeleted(id);
     }
     [TestMethod]
-    public void TestHasPerm_AddPerm()
+    public void TestHasPermission()
     {
       try
       {
@@ -41,26 +41,35 @@ namespace ZSZ.ServiceTest
         string roleName = "ForTest_" + Guid.NewGuid().ToString();
         long roleId = roleService.AddNew(roleName);
 
-        string userPhone = "ForTest_" + Guid.NewGuid().ToString().Substring(0,5);
-        long userId = adminUserService.AddAdminUser("ForTest", userPhone, "123", "@", null);
+        string userPhone = "ForTest_" + Guid.NewGuid().ToString().Substring(0, 5);
+        long userId = auService.AddAdminUser("ForTest", userPhone, "123", "@", null);
 
         roleService.AddRoleIds(userId, new long[] { roleId });
         permService.AddPermIds(roleId, new long[] { permId });
-        
-        Assert.IsTrue(adminUserService.HasPermission(userId, permName));
-        Assert.IsTrue(adminUserService.HasPermission(userId, permName2));
 
+        Assert.IsTrue(auService.HasPermission(userId, permName));
+        Assert.IsFalse(auService.HasPermission(userId, permName2));
       }
       catch (DbEntityValidationException ex)
       {
-        foreach (var item in ex.EntityValidationErrors.SelectMany(err=>err.ValidationErrors))
+        foreach (var item in ex.EntityValidationErrors.SelectMany(err => err.ValidationErrors))
         {
           Console.WriteLine(item.ErrorMessage);
-          
+
         }
         throw new Exception();
       }
     }
-
+    [TestMethod]
+    public void TestUpdateAdminUser()
+    {
+      //case: Update the existed and not deleted admin user
+      auService.UpdateAdminUser(1, "abc1", "111111", "1234", "ss", null);
+      //case: Check 
+      //case: Try to login with old authorization
+      //case: Try to login with new authorization
+      //case: Update the deleted admin user
+      //case: Update a not existed admin user      
+    }
   }
 }
