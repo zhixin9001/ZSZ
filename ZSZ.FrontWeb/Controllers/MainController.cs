7ebd1bfa38c51273044907de24a99b169f466f75
zhixin9001 <zhixin9001@126.com>
2017-05-03 07:43:53 +0800

    finish the preparation for front web

--- ZSZ.FrontWeb/Controllers/MainController.cs
+++ ZSZ.FrontWeb/Controllers/MainController.cs
@@ -9,20 +9,10 @@ namespace ZSZ.FrontWeb.Controllers
 {
   public class MainController : Controller
   {
-    public ICityService iCityService { get; set; }
     // GET: Main
     public ActionResult Index()
     {
-      //iCityService.AddNew("北京");
-
-      return Content(Session["ssk"].ToString());
-    }
-
-    public ActionResult Index1()
-    {
-      //iCityService.AddNew("北京");
-      Session["ssk"] = "ssk";
-      return Content("OK");
+      return View();
     }
   }
 }
